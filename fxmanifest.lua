f9225a686fde154e92d3fd2566be9bca7117ef48
ewony14 <86762474+ewony14@users.noreply.github.com>
2022-03-14 13:38:25 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+server_scripts {
+	'sv_main.lua'
+}
+
+client_scripts {
+	'cl_main.lua',
+}
+
+
+files({
+	"html/html.html",
+	"html/style.css",
+	"html/Heebo-Bold.ttf",
+	"html/app.js"
+})
+
+ui_page "html/html.html"
